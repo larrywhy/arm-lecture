3af32a71efa4d27090f8a1da2ca6c6a6062c1457
Sirius Wang <larrywhy.tw@gmail.com>
2015-03-12 21:00:09 +0800

    non-recursive fibonacci

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -9,28 +9,30 @@
 	.type fibonacci, function
 
 fibonacci:
-	@ ADD/MODIFY CODE BELOW
-	@ PROLOG
 	push {r3, r4, r5, lr}
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+	subs	r4, r0, #0
+	ble	.L3
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+	cmp	r4, #1
+	beq	.L4
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
+	subs	r3, r4, #1
+	mov 	r5, #0
+	mov	r6, #1
+loop:
+	adds	r7, r5, r6
+	mov	r5, r6
+	mov	r6, r7
 
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
+	subs	r3, r3, #1	@loop 
+	bne 	loop
 
-	@ R0 = R5 + R0 (update flags)
+	mov r0, r7
 
 	pop {r3, r4, r5, pc}		@EPILOG
 
-	@ END CODE MODIFICATION
+
 .L3:
 	mov r0, #0			@ R0 = 0
 	pop {r3, r4, r5, pc}		@ EPILOG
